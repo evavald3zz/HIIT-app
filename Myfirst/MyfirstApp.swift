a575d9bcedb2d2c18b4336defcfb4898d49c9641
evavald3zz <3vavaldez@gmail.com>
2025-01-20 20:20:43 -0500

    Initial Commit

---
+++ Myfirst/MyfirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyfirstApp.swift
+//  Myfirst
+//
+//  Created by M1_Tugo on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MyfirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyfirstTests/MyfirstTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyfirstTests.swift
+//  MyfirstTests
+//
+//  Created by M1_Tugo on 1/20/25.
+//
+
+import Testing
+@testable import Myfirst
+
+struct MyfirstTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
